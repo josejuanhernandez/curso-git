49bfa1e711e3bd471bcfbf8a8bcbf018485ba804
Jose Juan Hernandez <josejuanhernandez@gmail.com>
2021-04-21 15:04:41 +0100

    Age

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
+	private final static int age = 2;
 
-
-	
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+		System.out.println("Adios mundo");
+	}
 }
