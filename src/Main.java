621dcc18a1550bb8b266cb169657227932afa557
Jose Juan Hernandez <josejuanhernandez@gmail.com>
2021-04-22 14:48:09 +0100

    Scanner en main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
 	private final static int age = 2;
 	public static final String name = "Main";
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		Scanner scanner = new Scanner(System.in);
+		scanner.next();
 		System.out.println("Adios mundo");
 	}
 
