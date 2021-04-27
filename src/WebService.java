0c0eab0e2043e6d991a0ca3993d2d350e8517b51
JUAN RENE RAMIREZ GONZALEZ <jr.ramirezg@outlook.com>
2021-04-27 08:57:36 -0500

    constante usuario creado

--- src/WebService.java
+++ src/WebService.java
@@ -1,6 +1,7 @@
 public class WebService {
 	private final int port;
 	private static final String ip = "127.0.0.1";
+	private static final String usuario = "9epw9";
 
 	public WebService(int port) {
 		this.port = port;
