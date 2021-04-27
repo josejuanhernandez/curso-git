e8a48be5fe9a2317471aca8dea4ca2c13e4d5eb5
CFE\9L69A <daniel.caro@cfe.mx>
2021-04-27 09:22:54 -0500

    Se Agrega ip de Servidor Web Service

--- src/WebService.java
+++ src/WebService.java
@@ -3,6 +3,7 @@ public class WebService {
 	private static final String ip = "127.0.0.1";
 	private static final String usuario = "9epw9";
 	private static final String password = "12345*";
+	private static final String ip = "10.146.2.236";
 
 	public WebService(int port) {
 		this.port = port;
