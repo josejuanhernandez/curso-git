a45d971d72f166abccf8af2e899ab445a61f673d
CFE\9JFKA <ruth.alcazar@cfe.mx>
2021-04-27 12:53:54 -0500

    Cambio password

--- src/WebService.java
+++ src/WebService.java
@@ -2,7 +2,8 @@ public class WebService {
 	private final int port;
 	private static final String ip = "127.0.0.1";
 	private static final String user = "9epw9";
-	private static final String password = "12345*";
+//	private static final String password = "12345*";
+	private static final String password = "12345"
 //	private static final String ip = "10.146.2.236";
 
 	public WebService(int port) {
