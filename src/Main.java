a67c14cd97289843e3b3e4a44ffca107b5ae90b2
Jose Juan Hernandez <josejuanhernandez@gmail.com>
2021-04-27 15:15:25 +0100

    mode en Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
 	public static final String name = "Main";
+	public static final int mode = 1;
 
 	public static void main(String[] args) {
 		System.out.println("Web service");
